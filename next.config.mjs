d2cd1c5d3100225b321e6f5339be97c29bb675b7
Tristan Hollman <tristanhollman@users.noreply.github.com>
2023-09-21 22:29:45 +0200

    start github pages setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    loader: "akamai",
+    path: "",
+  },
   reactStrictMode: true,
 };
 
-module.exports = nextConfig;
+export default nextConfig;
